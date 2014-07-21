d288ade1905f3f99ec5d1c1c4f8e15fb8c18b336
Julia <schwarzkruppzo@gmail.com>
2014-07-21 14:24:14 +0400

    Removed shit

--- lua/autorun/libs/sv_hostname.lua
+++ lua/autorun/libs/sv_hostname.lua
@@ -9,34 +9,12 @@
 //===================================================================================
 
 bmas.hostname = {}
-bmas.hostname_prefix = "Command Center 'Gondor' - "
+bmas.hostname_prefix = "Your Prefix Here - "
 bmas.hostname = {
-	"Samo suschestvovanie INRI delaet nas vseh lashkami.",
-	"hello garry, why you are so stupid?",
-	"bad argument #1: nil expected, got string",
-	"PLZ UNBAN ME!11 plz plz",
-	"ubi dead. demon stand by",
-	"because this is 1337 variant to VAC ban a guy",
-	"how to noclip",
-	"what here to do?",
-	"Garry's mod is in chaos.",
-	"developers are pink",
-	"tushenka amerikanskaya - 300 rubles",
-	"I GOT HACKED BY WATCHDOGS!111!11",
-	"telephone watchdogs hacking servers",
-	"bitch mode enabled for tostring(ply)",
-	"bad argument #228: string expected, got string",	
-	"oh no!11 capster here!11 panic!11",
-	"INRI zapreshen konvenciey",
-	"WHAT A FUCK BOOOOOOOOOOOOOOOOOOOOOOM",
-	"Fertnon can't save the gmod.",
-	"he is doing addon for coderhire",
-	"YOU ARE BANNED WITH REASON TEST",
-	"Capster is a god.",
-	"Fertnon! Help, i got hacked by INRI.",
-	"do you like sandvichezzzz?",
-	"FUS-ROH-DAH!!!!!!!!!PIECE OF SHIT OMG OMG",
-	"warning, unauthorized biological forms detected"
+	"Your Text Here 1",
+	"Your Text Here 2",
+	"Your Text Here 4",
+	"Your Text Here 5"
 }
 bmas.hostname_delay = 3 // 3 seconds
 bmas.hostname_enabled = true
